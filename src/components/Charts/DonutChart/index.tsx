2a7f8480116aaa6c0fc09a093e4e73fb941cfc85
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2021-06-23 18:10:19 -0300

    Implement donut clicking

--- src/components/Charts/DonutChart/index.tsx
+++ src/components/Charts/DonutChart/index.tsx
@@ -3,6 +3,7 @@ import * as d3 from "d3";
 
 import { useData } from "hooks/DataContext";
 import SVGTooltip from "components/SVGTooltip";
+import { useSelection } from 'hooks/SelectionContext';
 
 interface IProps {
   data?: Data[];
@@ -20,9 +21,14 @@ const DonutChart: React.FC<IProps> = () => {
   const [data, setData] = useState<Data[]>([]);
   const { colors } = useData();
 
+  const { cad, changeSelection } = useSelection();
+
   // Valor entre (0, 1) para o quão grosso devem ser as fatias
   // em % do raio (1 = 100% do raio, 0 = 0% do raio)
   const thickness = 0.25;
+
+  // Grossura do destaque selecionado
+  const selectThickness = 2;
 
   const margins = {
     left: 0,
@@ -53,16 +59,18 @@ const DonutChart: React.FC<IProps> = () => {
       const arc = d3
         .arc<d3.PieArcDatum<Data>>()
         .innerRadius(radius * (1 - thickness))
-        .outerRadius(radius);
+        .outerRadius(radius - selectThickness / 2);
 
       svg
         .selectAll("path.slice")
         .data(arcs)
         .join("path")
-        .attr("class", "slice")
         .attr("transform", `translate(${margins.left + width / 2}, ${margins.top + height / 2})`)
-        .attr("fill", (d) => colors["cadeias"][d.data.ID.toString()]["color"])
-        .attr("d", arc)
+        .attr("class", "slice")
+        .style('cursor', 'pointer')
+        .on("click", (_, d) => {
+          changeSelection("cad", d.data.ID);
+        })
         .on("mouseover", (_, d) => {
           let [x, y] = arc.centroid(d);
           x += margins.left + width / 2;
@@ -75,9 +83,15 @@ const DonutChart: React.FC<IProps> = () => {
           );
           tooltip.show();
         })
-        .on("mouseleave", () => tooltip.hide());
+        .on("mouseleave", () => tooltip.hide())
+        .transition()
+        .duration(300)
+        .attr("d", arc)
+        .attr('stroke', 'black')
+        .attr('stroke-width', d => d.data.ID === cad ? selectThickness : 0)
+        .attr("fill", (d) => colors["cadeias"][d.data.ID.toString()]["color"]);
     }
-  }, [d3Container.current, data]);
+  }, [d3Container.current, data, cad]);
 
   useEffect(() => {
     // TODO: Fetch data from the server
@@ -88,7 +102,7 @@ const DonutChart: React.FC<IProps> = () => {
       { Valor: 3, ID: 4 },
       { Valor: 2, ID: 5 }
     ]);
-  }, [1]);
+  }, []);
 
   return <svg ref={d3Container} width="100%" height="100%" />;
 };
