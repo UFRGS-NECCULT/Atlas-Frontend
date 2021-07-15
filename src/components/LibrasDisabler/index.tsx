cbd17e07d0eb38f7c7f99713bc86eb427277af35
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2021-07-15 17:28:35 -0300

    Desabilitar VLibras no /resultado

---
+++ src/components/LibrasDisabler/index.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+export const LibrasDisabler: React.FC = ({ children }) => {
+  const VLibras = (window as any).VLibras;
+
+  // Rodar quando este componente for montado
+  useEffect(() => {
+    if (VLibras.element) {
+      VLibras.element.classList.remove("enabled");
+    }
+  }, []);
+
+  // Rodar quando este componente for desmontado
+  useEffect(() => {
+    return () => {
+      if (VLibras.element) {
+          VLibras.element.classList.add("enabled");
+      }
+    };
+  }, []);
+
+  return <>{children}</>;
+};
