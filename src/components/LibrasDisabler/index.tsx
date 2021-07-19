e458fb406d495e48bea15219c1000a421ae374be
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2021-07-19 17:27:52 -0300

    Ao carregar a página, desabilitar o VLibras

--- src/components/LibrasDisabler/index.tsx
+++ src/components/LibrasDisabler/index.tsx
@@ -1,25 +1,40 @@
 import React, { useEffect } from "react";
 
+const disable = () => {
+  const VLibras = (window as any).VLibras;
+
+  if (VLibras && VLibras.element) {
+    VLibras.element.classList.remove("enabled");
+  }
+};
+
+const enable = () => {
+  const VLibras = (window as any).VLibras;
+
+  if (VLibras && VLibras.element) {
+      VLibras.element.classList.add("enabled");
+  }
+};
+
+const onDocumentReadyStateChange = () => {
+  if (document.readyState === 'complete') {
+    // HACK: Esperar 200 ms para desabilitar
+    setTimeout(disable, 200);
+  }
+}
+
 export const LibrasDisabler: React.FC = ({ children }) => {
 
   // Rodar quando este componente for montado
   useEffect(() => {
-    const VLibras = (window as any).VLibras;
-
-    if (VLibras && VLibras.element) {
-      VLibras.element.classList.remove("enabled");
-    }
+    document.addEventListener('readystatechange', onDocumentReadyStateChange);
+    disable();
   }, []);
 
   // Rodar quando este componente for desmontado
-  useEffect(() => {
-    return () => {
-      const VLibras = (window as any).VLibras;
-
-      if (VLibras && VLibras.element) {
-          VLibras.element.classList.add("enabled");
-      }
-    };
+  useEffect(() => () => {
+    document.removeEventListener('readystatechange', onDocumentReadyStateChange);
+    enable();
   }, []);
 
   return <>{children}</>;
