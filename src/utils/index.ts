225de4b4acca89c53a103c837245a05d88ce489b
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2021-08-23 16:18:16 -0300

    RichStrings com nunjucks

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
 import * as d3 from "d3";
+import { useSelection } from 'hooks/SelectionContext';
+import nunjucks from "nunjucks";
 
 // Local pt-BR
 const locale = d3.formatLocale({
@@ -32,4 +34,70 @@ export function formatter(type: string): (n: number) => string {
 export function format(value: number, type: string): string {
   const f = formatter(type);
   return f(value);
+}
+
+/**
+ *
+ * @param template Template para substituição
+ * @returns Objeto contendo a string resultante e um vetor informando quais strings variáveis foram inseridas
+ * @example richString("Dados {{ uf if uf else 'do Brasil' }}")
+ */
+export function richString(template: string) {
+  const { config, uf, cad, ano, deg, prc } = useSelection();
+  // TODO: ocp no useSelection()
+  const ocp = 0;
+
+  // Mapa de variáveis no formato "nomeVar" => valorVar
+  const variables = {
+    uf,
+    cad,
+    ano,
+    deg,
+    prc,
+    ocp
+  };
+
+  // Pegar os dados das variáveis selecionadas para construir o contexto
+  const context: any = {};
+  for (const varName in variables) {
+    const data = config.breadcrumbs.find((b) => b.id === varName)?.options.find((o) => o.id === variables[varName]);
+
+    if (data && data.id !== 0) {
+      context[varName] = data.display ? data.display : data.nome;
+    }
+  }
+
+  // Modificar o contexto para registrar quais variáveis foram acessadas
+  const accessed: any = {};
+  const renderContext: any = {};
+  for (const key in context) {
+    accessed[key] = false;
+
+    if (context[key]) {
+      renderContext[key] = {
+        // Converter para string indica que esse objeto foi inserido no template
+        toString() {
+          accessed[key] = true;
+          return context[key].toString();
+        }
+      };
+    }
+  }
+
+  const str = nunjucks.renderString(template, renderContext);
+
+  // Guarde o nome das variáveis utilizadas
+  const used: string[] = [];
+  for (const key in accessed) {
+    if (accessed[key]) {
+      used.push(key);
+    }
+  }
+
+  const res = {
+    string: str,
+    used
+  };
+
+  return res;
 }
