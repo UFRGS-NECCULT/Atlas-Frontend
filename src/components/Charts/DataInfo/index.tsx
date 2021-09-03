a080027daa7d734166addc121a7af75407736345
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2021-09-03 16:06:17 -0300

    Refatorar DataInfo

--- src/components/Charts/DataInfo/index.tsx
+++ src/components/Charts/DataInfo/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelection } from "hooks/SelectionContext";
-import { useData } from "hooks/DataContext";
+import { IDescriptions, useData } from "hooks/DataContext";
 import { TabButton, Flex, Column, BigNumber, BigNumberDesc, Container, Source, Row } from "./styles";
 import { getInfo } from "services/api";
 
@@ -46,6 +46,10 @@ interface DataPoint {
   id_tipo?: number;
   nome_tipo?: string;
   display_subdeg?: string;
+
+  // Valor que indica se um valor deve ser mostrado de maneira absoluta
+  // ou como porcentagem do valor principal (padrão: porcentagem)
+  display_absolute?: boolean;
 }
 
 interface ChartProps {
@@ -64,11 +68,11 @@ const DataInfo: React.FC<ChartProps> = ({ constants }) => {
 
   const [data, setData] = useState<Data | null>(null);
 
-  let tab: 0 | 1 = 0;
+  let tab: 1 | 2 = 1;
 
   // Escolher aba correta
   if (eixo === 2 && ocp !== 0) {
-    tab = 1;
+    tab = 2;
   }
 
   useEffect(() => {
@@ -79,7 +83,7 @@ const DataInfo: React.FC<ChartProps> = ({ constants }) => {
     };
 
     getData();
-  }, [eixo, num, ano, cad, uf, deg, ocp, prc, cns, tpo, mec, pfj, config]);
+  }, [eixo, ano, num, cad, uf, deg, ocp, prc, cns, tpo, mec, pfj, config]);
 
   const tabs = (data: Data) => {
     // Só eixos do Mercado, Fomento e Comércio Internacional têm abas
@@ -96,133 +100,50 @@ const DataInfo: React.FC<ChartProps> = ({ constants }) => {
 
     return (
       <Flex>
-        <TabButton className={tab === 0 ? "active" : ""} style={{ backgroundColor: config.primaryColor }}>
+        <TabButton className={tab === 1 ? "active" : ""} style={{ backgroundColor: config.primaryColor }}>
           {leftButtonText}
         </TabButton>
-        <TabButton className={tab === 1 ? "active" : ""} style={{ backgroundColor: config.primaryColor }}>
+        <TabButton className={tab === 2 ? "active" : ""} style={{ backgroundColor: config.primaryColor }}>
           {rightButtonText}
         </TabButton>
       </Flex>
     );
   };
 
-  // Acha a entrada correta para um valor baseando-se na seleção
-  // de valores do breadcrumb
-  const findSelectedData = (data: Data) => {
-    const d = data.data.find(
-      (d) =>
-        d.id_uf === data.selection.uf &&
-        d.id_cad === data.selection.cad &&
-        (d.id_subdeg !== undefined ? d.id_subdeg === data.selection.deg : true) &&
-        (d.id_ocupacao !== undefined ? d.id_ocupacao === data.selection.ocp : true) &&
-        (d.id_parceiro !== undefined ? d.id_parceiro === data.selection.prc : true) &&
-        (d.id_tipo !== undefined ? d.id_tipo === data.selection.tpo : true) &&
-        (d.id_consumo !== undefined ? d.id_consumo === data.selection.cns : true)
-    );
+  const displayValues = (data: Data) => {
+    // Pegar descrição e valor dos números
+    const main = getValue(desc, tab, data, 1);
+    const scnd = getValue(desc, tab, data, 2);
+    const thrd = getValue(desc, tab, data, 3);
 
-    return d || null;
-  };
-
-  // Acha a entrada correta para um valor baseando-se nas expressões
-  // de substituição encontradas na string que descreve esse valor
-  const findCorrectData = (filters: string[], data: Data) => {
-    // Atalho para deixar o código mais enxuto
-    // Diz se um nome está presente na lista de filtros
-    const i = (name) => filters.includes(name);
-
-    const d = data.data.find(
-      (d) =>
-        d.id_uf === (i("uf") ? data.selection.uf : 0) &&
-        d.id_cad === (i("cad") ? data.selection.cad : 0) &&
-        (d.id_subdeg !== undefined ? d.id_subdeg === (i("deg") ? data.selection.deg : 0) : true) &&
-        (d.id_ocupacao !== undefined ? d.id_ocupacao === (i("ocp") ? data.selection.ocp : 0) : true) &&
-        (d.id_parceiro !== undefined ? d.id_parceiro === (i("prc") ? data.selection.prc : 0) : true) &&
-        (d.id_consumo !== undefined ? d.id_consumo === (i("cns") ? data.selection.cns : 0) : true) &&
-        (d.id_tipo !== undefined && i("tpo") ? d.id_tipo === data.selection.tpo : true) // Tipo não tem um "filtro total"
-    );
-
-    return d || null;
-  };
-
-  const displayValues = () => {
-    if (!data) {
-      return false;
-    }
-
-    // Se não há definição para esses valores, não mostre nada
-    const tabs = desc[data.selection.eixo - 1][data.selection.num.toString()];
-    const descriptions = tabs ? tabs[tab] : null;
-    if (!descriptions || !descriptions[0] || typeof descriptions[0] !== "string") {
-      return false;
-    }
-
-    // Pegar descrição e valor do número principal
-    let mainStr = "";
-    let main: DataPoint | null = null;
-    if (shouldDisplayDescription(data.selection.eixo, data.selection.num, tab + 1, 1, data.selection)) {
-      if (descriptions[0] && typeof descriptions[0] === "string") {
-        const rich = richString(descriptions[0], data.selection);
-        mainStr = rich.string;
-        main = findCorrectData(rich.used, data);
-      } else if (descriptions[0] === "") {
-        main = findSelectedData(data);
-      }
-    }
-
-    // Pegar descrição e valor do segundo valor
-    let scndStr = "";
-    let scnd: DataPoint | null = null;
-    if (shouldDisplayDescription(data.selection.eixo, data.selection.num, tab + 1, 2, data.selection)) {
-      if (descriptions[1] && typeof descriptions[1] === "string") {
-        const rich = richString(descriptions[1], data.selection);
-        scndStr = rich.string;
-        scnd = findCorrectData(rich.used, data);
-      } else if (descriptions[0] === "") {
-        scnd = findSelectedData(data);
-      }
-    }
-
-    // Pegar descrição e valor do terceiro valor
-    let thrdStr = "";
-    let thrd: DataPoint | null = null;
-    if (shouldDisplayDescription(data.selection.eixo, data.selection.num, tab + 1, 3, data.selection)) {
-      if (descriptions[2] && typeof descriptions[2] === "string") {
-        const rich = richString(descriptions[2], data.selection);
-        thrdStr = rich.string;
-        thrd = findCorrectData(rich.used, data);
-      } else if (descriptions[2] === "") {
-        thrd = findSelectedData(data);
-      }
-    }
-
-    return [
-      main && (
+    return (
+      main && [
         <Column key="0">
-          <BigNumber style={{ color: config.primaryColor }}>{format(main.valor, main.formato)}</BigNumber>
-          <BigNumberDesc>{mainStr}</BigNumberDesc>
-        </Column>
-      ),
-      (scnd || thrd) && (
-        <Row key="1">
-          {scnd && (
-            <Column>
-              <BigNumber style={{ color: config.primaryColor }}>
-                {format((main?.valor || 0) / scnd.valor, "percent")}
-              </BigNumber>
-              <BigNumberDesc>{scndStr}</BigNumberDesc>
-            </Column>
-          )}
-          {thrd && (
-            <Column>
-              <BigNumber style={{ color: config.primaryColor }}>
-                {format((main?.valor || 0) / thrd.valor, "percent")}
-              </BigNumber>
-              <BigNumberDesc>{thrdStr}</BigNumberDesc>
-            </Column>
-          )}
-        </Row>
-      )
-    ];
+          <BigNumber style={{ color: config.primaryColor }}>{format(main.data.valor, main.data.formato)}</BigNumber>
+          <BigNumberDesc>{main.string}</BigNumberDesc>
+        </Column>,
+        (scnd || thrd) && (
+          <Row key="1">
+            {scnd && (
+              <Column>
+                <BigNumber style={{ color: config.primaryColor }}>
+                  {format(main.data.valor / scnd.data.valor, "percent")}
+                </BigNumber>
+                <BigNumberDesc>{scnd.string}</BigNumberDesc>
+              </Column>
+            )}
+            {thrd && (
+              <Column>
+                <BigNumber style={{ color: config.primaryColor }}>
+                  {format(main.data.valor / thrd.data.valor, "percent")}
+                </BigNumber>
+                <BigNumberDesc>{thrd.string}</BigNumberDesc>
+              </Column>
+            )}
+          </Row>
+        )
+      ]
+    );
   };
 
   if (!data || !data.data.length) {
@@ -232,10 +153,83 @@ const DataInfo: React.FC<ChartProps> = ({ constants }) => {
   return (
     <>
       {tabs(data)}
-      <Container>{displayValues()}</Container>
+      <Container>{displayValues(data)}</Container>
       <Source>Fonte: {data.data[0].fonte ? data.data[0].fonte : "Sem fonte"}</Source>
     </>
   );
 };
 
 export default DataInfo;
+
+// Retorna o número e a descrição que deve ser mostrado no n-ésimo slot do datainfo
+function getValue(desc: IDescriptions, tab: 1 | 2, data: Data, val: 1 | 2 | 3) {
+  // Se não há definição para esses valores, não mostre nada
+  const tabs = desc[data.selection.eixo - 1][data.selection.num.toString()];
+  const descriptions = tabs ? tabs[tab - 1] : null;
+  if (!descriptions || !descriptions[val - 1]) {
+    return null;
+  }
+
+  if (shouldDisplayDescription(data.selection.eixo, data.selection.num, tab, val, data.selection)) {
+    if (descriptions[val - 1] && typeof descriptions[0] === "string") {
+      const rich = richString(descriptions[val - 1], data.selection);
+      const valData = findCorrectData(rich.used, data);
+
+      if (valData) {
+        return {
+          string: rich.string,
+          data: valData
+        };
+      }
+    } else if (descriptions[0] === "") {
+      const valData = findSelectedData(data);
+
+      if (valData) {
+        return {
+          data: valData,
+          string: ""
+        };
+      }
+    }
+  }
+
+  return null;
+}
+
+// Acha a entrada correta para um valor baseando-se nas expressões
+// de substituição encontradas na string que descreve esse valor
+function findCorrectData(filters: string[], data: Data) {
+  // Atalho para deixar o código mais enxuto
+  // Diz se um nome está presente na lista de filtros
+  const i = (name) => filters.includes(name);
+
+  const d = data.data.find(
+    (d) =>
+      d.id_uf === (i("uf") ? data.selection.uf : 0) &&
+      d.id_cad === (i("cad") ? data.selection.cad : 0) &&
+      (d.id_subdeg !== undefined ? d.id_subdeg === (i("deg") ? data.selection.deg : 0) : true) &&
+      (d.id_ocupacao !== undefined ? d.id_ocupacao === (i("ocp") ? data.selection.ocp : 0) : true) &&
+      (d.id_parceiro !== undefined ? d.id_parceiro === (i("prc") ? data.selection.prc : 0) : true) &&
+      (d.id_consumo !== undefined ? d.id_consumo === (i("cns") ? data.selection.cns : 0) : true) &&
+      (d.id_tipo !== undefined && i("tpo") ? d.id_tipo === data.selection.tpo : true) // Tipo não tem um "filtro total"
+  );
+
+  return d || null;
+}
+
+// Acha a entrada correta para um valor baseando-se na seleção
+// de valores do breadcrumb
+function findSelectedData(data: Data) {
+  const d = data.data.find(
+    (d) =>
+      d.id_uf === data.selection.uf &&
+      d.id_cad === data.selection.cad &&
+      (d.id_subdeg !== undefined ? d.id_subdeg === data.selection.deg : true) &&
+      (d.id_ocupacao !== undefined ? d.id_ocupacao === data.selection.ocp : true) &&
+      (d.id_parceiro !== undefined ? d.id_parceiro === data.selection.prc : true) &&
+      (d.id_tipo !== undefined ? d.id_tipo === data.selection.tpo : true) &&
+      (d.id_consumo !== undefined ? d.id_consumo === data.selection.cns : true)
+  );
+
+  return d || null;
+}
